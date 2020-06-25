5a3102f9cc508fff1d582b5e97814b1b13df7110
Marceau Maubert <marceau.maubert2@gmail.com>
2020-06-25 20:45:40 +0200

    make sure this is a thing

--- scripts/github-repository-navbar-fix.js
+++ scripts/github-repository-navbar-fix.js
@@ -8,19 +8,24 @@
 // @grant        none
 // ==/UserScript==
 
-const repohead = document.querySelector(".repohead");
-const children = repohead.children;
-const containerXl = document.createElement("div");
-containerXl.className = "container-xl";
-while (repohead.childNodes.length > 0) {
-	containerXl.appendChild(repohead.childNodes[0]);
-}
-repohead.appendChild(containerXl);
+function fixNavbar() {
+	const fixed = document.querySelector(".repohead .container-xl");
+	if (fixed) return;
+	const repohead = document.querySelector(".repohead");
+	const containerXl = document.createElement("div");
+	containerXl.className = "container-xl";
+	while (repohead.childNodes.length > 0) {
+		containerXl.appendChild(repohead.childNodes[0]);
+	}
+	repohead.appendChild(containerXl);
 
-const underlineNav = document.querySelector(".repohead .UnderlineNav");
-underlineNav.style = "overflow: visible !important";
-const underlineNavOverflow = document.querySelector(
-	".repohead .UnderlineNav [class*='underlinenav-overflow']"
-);
-underlineNavOverflow.style =
-	"position: relative !important; padding-right: 0 !important;";
+	const underlineNav = document.querySelector(".repohead .UnderlineNav");
+	underlineNav.style = "overflow: visible !important";
+	const underlineNavOverflow = document.querySelector(
+		".repohead .UnderlineNav [class*='underlinenav-overflow']"
+	);
+	underlineNavOverflow.style =
+		"position: relative !important; padding-right: 0 !important;";
+}
+fixNavbar();
+setInterval(fixNavbar, 0);
